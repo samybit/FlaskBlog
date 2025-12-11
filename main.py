90905c1da2b54c9bd9290743a602ab09d2fe8622
samybit <samyb.samir@gmail.com>
2025-12-11 16:23:35 +0200

    Upgrade all via Bootstrap template and unsplash pics

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
 from flask import Flask, render_template
-from post import Post
 import requests
+from datetime import datetime
 
-url = "https://api.npoint.io/abfbbb2fb46d6ccd9494"
-response = requests.get(url)
-posts = response.json()
-
-post_objects = []
-for post in posts:
-    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
-    post_objects.append(post_obj)   
+# get all posts
+all_posts = requests.get("https://api.npoint.io/abfbbb2fb46d6ccd9494").json()
 
 app = Flask(__name__)
 
+# get and inject current year
+@app.context_processor
+def inject_now():
+    return {'year': datetime.now().year}
+
 @app.route('/')
 def get_all_posts():
-    return render_template("index.html", all_posts=post_objects)
+    return render_template("index.html", posts=all_posts)
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
 
 @app.route("/post/<int:index>")
 def show_post(index):
     requested_post = None
-    for blog_post in post_objects:
-        if blog_post.id == index:
+    for blog_post in all_posts:
+        if blog_post["id"] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
