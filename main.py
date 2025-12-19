68c0a8fa278ee8c8dcb0a38f4b4aa08925d365b7
samybit <samyb.samir@gmail.com>
2025-12-19 12:57:45 +0200

    Final working project

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for
-import requests
+# Standard library imports
+import logging
+import os
+import smtplib
 from datetime import datetime
+
+# Third-party imports
+import requests
+from dotenv import load_dotenv
+from flask import Flask, redirect, render_template, request, url_for
+
+logging.basicConfig(level=logging.INFO)
+
+load_dotenv()
+
+MY_EMAIL = os.getenv("MY_EMAIL")
+APP_PASSWORD = os.getenv("APP_PASSWORD")
 
 # get all posts
 all_posts = requests.get("https://api.npoint.io/abfbbb2fb46d6ccd9494").json()
@@ -23,23 +37,35 @@ def about():
     return render_template("about.html")
 
 
+def send_email(name, email, phone, message):
+    email_message = f"Subject: Flask Blog message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}"
+
+    logging.info(f"Attempting to send email to {MY_EMAIL}...")
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, APP_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIL, msg=email_message)
+
+    logging.info("SMTP connection closed.")
+
 # contact GET
 @app.get('/contact')
 def contact_form():
     # check if message has been sent -> if the URL has "?success=True"
     msg_sent = request.args.get('success')
-
     # Pass status to the HTML
     return render_template("contact.html", msg_sent=msg_sent)
 
-# contact POST
+# contact POST and redirect
 @app.post('/contact')
 def contact_submit():
     data = request.form
-    print(data["name"])
-    print(data["email"])
-    print(data["phone"])
-    print(data["message"])
+    logging.info(f"Form received! Name: {data['name']}, Email: {data['email']}")
+
+    send_email(data["name"], data["email"], data["phone"], data["message"])
+    logging.info("Email sent successfully.")
+
     return redirect(url_for('contact_form', success=True))
 
 
